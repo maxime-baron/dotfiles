06a38eba0d1e8d03b450e8294a23c199693926f8
Maxime Baron <maxime.baron.dev@gmail.com>
2026-01-15 13:08:06 +0100

    Add reveal animation on mount + format

--- .config/ags/widget/Notification/Notification.tsx
+++ .config/ags/widget/Notification/Notification.tsx
@@ -4,7 +4,7 @@ import Adw from "gi://Adw"
 import GLib from "gi://GLib"
 import AstalNotifd from "gi://AstalNotifd"
 import Pango from "gi://Pango"
-import { onCleanup } from 'gnim'
+import { onCleanup, createState, onMount } from 'gnim'
 
 function isIcon(icon?: string | null) {
   const iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default()!)
@@ -39,9 +39,23 @@ interface NotificationProps {
 export default function Notification({ notification: n }: NotificationProps) {
   const timeoutDuration = n.urgency === AstalNotifd.Urgency.CRITICAL ? 5 : 3 // 5s for critical, 3s for others
 
+  const [revealed, setRevealed] = createState(false)
+  let isPaused = false
+
+
   // Timeout to automatically close the notification
-  const timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, timeoutDuration, () => {
-    n.dismiss()
+  const timeoutId = GLib.timeout_add_seconds(
+    GLib.PRIORITY_DEFAULT,
+    timeoutDuration,
+    () => {
+      n.dismiss()
+      return GLib.SOURCE_REMOVE
+    }
+  )
+
+  // Reveal animation on mount
+  GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+    setRevealed(true)
     return GLib.SOURCE_REMOVE
   })
 
@@ -49,72 +63,83 @@ export default function Notification({ notification: n }: NotificationProps) {
   onCleanup(() => GLib.source_remove(timeoutId))
 
   /**
- * Handles mouse click events on the notification.
- * Left click invokes the default action, middle click dismisses the notification.
- */
+   * Handles mouse click events on the notification.
+   * Left click invokes the default action, middle click dismisses the notification.
+   */
   const clickHanlder = (source: Gtk.GestureClick) => {
     const button = source.get_current_button()
-  
+
     switch (button) {
       case Gdk.BUTTON_PRIMARY: // Left click
-        n.invoke("default");
+        n.invoke("default")
         break
-        
-        case Gdk.BUTTON_MIDDLE: // Middle click
+
+      case Gdk.BUTTON_MIDDLE: // Middle click
         n.dismiss()
         break
 
-        default:
+      default:
         break
     }
   }
 
   return (
-    <Adw.Clamp maximumSize={258}>
-      <box
-        widthRequest={258}
-        class={`Notification ${urgency(n)}`}
-        orientation={Gtk.Orientation.VERTICAL}
-        cursor={Gdk.Cursor.new_from_name("pointer", null)}
-      >
-        <Gtk.GestureClick button={0} onPressed={clickHanlder}/>
-        <box class="header">
-          {(n.appIcon || isIcon(n.desktopEntry) || fileExists(n.image)) && (
-            <image
-              class="app-icon"
-              visible={Boolean(n.appIcon || n.desktopEntry || n.image)}
-              iconName={n.appIcon || n.desktopEntry}
-              {...(!n.appIcon && !isIcon(n.desktopEntry) && fileExists(n.image) ? { file: n.image } : {})}
-            />
-          )}
-          <label
-            class="summary"
-            halign={Gtk.Align.START}
-            ellipsize={Pango.EllipsizeMode.END}
-            label={n.summary || "Unknown"}
-          />
-          <label
-            class="time"
-            hexpand
-            halign={Gtk.Align.END}
-            label={time(n.time)}
-          />
-        </box>
-        <box class="content">
-          <box orientation={Gtk.Orientation.VERTICAL}>
-            {n.body && (
-              <label
-                class="body"
-                ellipsize={Pango.EllipsizeMode.END}
-                lines={2}
-                xalign={0}
-                wrap
-                label={n.body}
+    <Gtk.Revealer
+      name={`notif-${n.id}`}
+      revealChild={revealed}
+      transitionType={Gtk.RevealerTransitionType.SLIDE_LEFT}
+      transitionDuration={100}
+    >
+      <Adw.Clamp maximumSize={258}>
+        <box
+          widthRequest={258}
+          class={`Notification ${urgency(n)}`}
+          orientation={Gtk.Orientation.VERTICAL}
+          cursor={Gdk.Cursor.new_from_name("pointer", null)}
+        >
+          <Gtk.GestureClick button={0} onPressed={clickHanlder} />
+          <box class="header">
+            {(n.appIcon || isIcon(n.desktopEntry) || fileExists(n.image)) && (
+              <image
+                class="app-icon"
+                visible={Boolean(n.appIcon || n.desktopEntry || n.image)}
+                iconName={n.appIcon || n.desktopEntry}
+                {...(!n.appIcon &&
+                !isIcon(n.desktopEntry) &&
+                fileExists(n.image)
+                  ? { file: n.image }
+                  : {})}
               />
             )}
+            <label
+              class="summary"
+              halign={Gtk.Align.START}
+              ellipsize={Pango.EllipsizeMode.END}
+              label={n.summary || "Unknown"}
+            />
+            <label
+              class="time"
+              hexpand
+              halign={Gtk.Align.END}
+              label={time(n.time)}
+            />
+          </box>
+          <box class="content">
+            <box orientation={Gtk.Orientation.VERTICAL}>
+              {n.body && (
+                <label
+                  class="body"
+                  ellipsize={Pango.EllipsizeMode.END}
+                  lines={2}
+                  xalign={0}
+                  wrap
+                  label={n.body}
+                />
+              )}
+            </box>
           </box>
         </box>
-      </box>
-    </Adw.Clamp>
+      </Adw.Clamp>
+    </Gtk.Revealer>
   )
 }
