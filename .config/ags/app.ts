eebd9b425c6ed90c00da458942acbcfb45c89287
Maxime Baron <maxime.baron.dev@gmail.com>
2026-01-22 12:59:42 +0100

    Add Media player skeleton

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -2,10 +2,12 @@ import app from "ags/gtk4/app"
 import style from "./style.scss"
 import NotificationPopup from './widget/NotificationPopup/NotificationPopup'
 import NotificationList from './widget/NotificationList/NotificationList'
+import MediaPlayerPopup from './widget/MediaPlayer/MediaPlayerPopup'
 
 app.start({
   css: style,
   main() {
+    MediaPlayerPopup()
     NotificationList()
     NotificationPopup()
   },
