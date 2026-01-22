4197c952f24bfd72bf8e161efca76a433d05e434
Maxime Baron <maxime.baron.dev@gmail.com>
2026-01-22 12:59:42 +0100

    Add Media player skeleton

---
+++ .config/ags/widget/MediaPlayer/MediaPlayer.tsx
@@ -0,0 +1,78 @@
+import Gtk from "gi://Gtk?version=4.0"
+import Gdk from "gi://Gdk?version=4.0"
+import Adw from "gi://Adw"
+import Mpris from "gi://AstalMpris"
+import Pango from "gi://Pango"
+import { fileExists} from '../../utils/fsUtils'
+import GLib from 'gi://GLib?version=2.0'
+import { createBinding } from 'gnim'
+
+interface NotificationProps {
+  mediaPlayer: Mpris.Player
+}
+
+const iconPath = GLib.get_home_dir() + "/Documents/Projects/dotfiles/.config/waybar/icons/media-player/"
+
+export default function MediaPlayer({ mediaPlayer: mp }: NotificationProps) {
+  const timeoutDuration = 1 // 1 second
+
+  const playbackStatus = createBinding(mp, "playbackStatus");
+  const position = createBinding(mp, "position");
+  const length = createBinding(mp, "length"); 
+
+  return (
+      <Adw.Clamp maximumSize={258}>
+        <box
+          widthRequest={300}
+          class="media-player"
+          orientation={Gtk.Orientation.VERTICAL}
+          cursor={Gdk.Cursor.new_from_name("pointer", null)}
+        >
+          {/* <Gtk.GestureClick button={0} onPressed={clickHanlder} />
+          <Gtk.EventControllerMotion onEnter={() => stopTimeout()} onLeave={() => startTimeout()}/> */}
+          <box spacing={16}>
+            <box class="track-data" hexpand={true} spacing={8}>
+              {(fileExists(mp.get_cover_art())) && (
+                <image
+                  class="cover"
+                  visible={Boolean(mp.get_cover_art())}
+                  file={mp.get_cover_art()}
+                  pixelSize={48}
+                  overflow={Gtk.Overflow.HIDDEN}
+                />
+              )}
+              <box orientation={Gtk.Orientation.VERTICAL} valign={Gtk.Align.CENTER} spacing={2}>
+                <label
+                  class="title"
+                  halign={Gtk.Align.START}
+                  ellipsize={Pango.EllipsizeMode.END}
+                  label={mp.get_title() || "Unknown"}
+                />
+                <label
+                  class="artist"
+                  halign={Gtk.Align.START}
+                  ellipsize={Pango.EllipsizeMode.END}
+                  label={mp.get_artist() || "Unknown"}
+                />
+              </box>
+            </box>
+            <box class="track-actions" hexpand={false}>
+              <button onClicked={()=>mp.previous()}><image class="track-action" file={`${iconPath}media-player-backward.svg`} pixelSize={20}/></button>
+              <button onClicked={()=>mp.play_pause()}><image class="track-action" file={`${iconPath}${playbackStatus() === Mpris.PlaybackStatus.PLAYING ? 'media-player-pause.svg' : 'media-player-resume.svg'}`} pixelSize={20}/></button>
+              <button onClicked={()=>mp.next()}><image class="track-action" file={`${iconPath}media-player-forward.svg`} pixelSize={20}/></button>
+            </box>
+          </box>
+          <slider
+            orientation={Gtk.Orientation.HORIZONTAL}
+            // drawValue={true}
+            hexpand={true}
+            value={position}
+            min={0}
+            max={length}
+            step={5}
+            onChangeValue={(_, __, value) => {mp.position = value; return false}}
+          />
+        </box>
+      </Adw.Clamp>
+  )
+}
---
+++ .config/ags/widget/MediaPlayer/MediaPlayerPopup.tsx
@@ -0,0 +1,66 @@
+import app from "ags/gtk4/app"
+import { Astal, Gtk } from "ags/gtk4"
+import Mpris from "gi://AstalMpris"
+import { createBinding, For, createState, onCleanup, With } from "ags"
+import MediaPlayer from './MediaPlayer'
+
+export default function MediaPlayerPopup() {
+  const monitors = createBinding(app, "monitors")
+  const mpris = Mpris.get_default()
+
+  const [player, setPlayer] = createState<Mpris.Player | undefined>(undefined)
+  const [visibility, setVisibility] = createState<boolean>(false)
+  const notifyHandlers = new Map<Mpris.Player, number>()
+
+  const playerAddedHandler = mpris.connect("player-added", (_, addedPlayer) => {
+    console.log(addedPlayer.busName)
+    const notifyId = addedPlayer.connect('notify::playback-status', () => {
+      console.log(addedPlayer.busName)
+      setPlayer(addedPlayer)
+      setVisibility(true)
+    })
+    
+    notifyHandlers.set(addedPlayer, notifyId)
+  })
+
+  const playerClosedHandler = mpris.connect("player-closed", (_, closedPlayer) => {
+    // Disconnect the notify signal
+    const notifyId = notifyHandlers.get(closedPlayer)
+    if (notifyId !== undefined) {
+      closedPlayer.disconnect(notifyId)
+      notifyHandlers.delete(closedPlayer)
+    }
+    
+    setPlayer((currentPlayer) => {
+      // Check if the closedPlayer is in state
+      if (currentPlayer === closedPlayer) {
+        return undefined
+      }
+      return currentPlayer
+    })
+  })
+
+  onCleanup(() => {
+    mpris.disconnect(playerAddedHandler)
+    mpris.disconnect(playerClosedHandler)
+  })
+
+  return (
+    <For each={monitors}>
+      {(monitor) => (
+        <window
+          $={(self) => onCleanup(() => self.destroy())}
+          class="mediaplayer-popups"
+          namespace="notification-popup"
+          gdkmonitor={monitor}
+          visible={visibility}
+          anchor={Astal.WindowAnchor.TOP | Astal.WindowAnchor.RIGHT}
+        >
+          <With value={player}>
+            {(mediaPlayer)=> mediaPlayer && <MediaPlayer mediaPlayer={mediaPlayer}/>}
+          </With>
+        </window>
+      )}
+    </For>
+  )
+}
